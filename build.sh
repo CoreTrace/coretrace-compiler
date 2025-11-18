523740c0c3e1375d90a1e3572c910a7f13de45e4
Hugo <hugo.payet@epitech.eu>
2025-11-19 02:33:07 +0900

    chore(build.sh): update LLVM version

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cmake .. -DCMAKE_BUILD_TYPE=Release \
-         -DLLVM_DIR=$(brew --prefix llvm)/lib/cmake/llvm \
-         -DClang_DIR=$(brew --prefix llvm)/lib/cmake/clang \
+         -DLLVM_DIR=$(brew --prefix llvm@20)/lib/cmake/llvm \
+         -DClang_DIR=$(brew --prefix llvm@20)/lib/cmake/clang \
          -DUSE_SHARED_LIB=OFF \
 && make -j4
