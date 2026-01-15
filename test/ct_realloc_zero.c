1ac297385e5ba0bb69bef05ad89b09ece405e3a5
Hugo <hugo.payet@epitech.eu>
2026-01-15 09:34:27 +0100

    Tests: Add vtable tests and diagnostic warning fixtures

--- test/ct_realloc_zero.c
+++ test/ct_realloc_zero.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
-int main(void) {
+int main(void)
+{
     char *p = (char *)malloc(32);
     p = (char *)realloc(p, 0); // free-like behavior per libc
     (void)p;
---
+++ test/ct_vtable_diag_null.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+extern "C" void __ct_vtable_dump(void *this_ptr, const char *site, const char *static_type);
+
+int main()
+{
+    std::puts("ct_vtable_diag_null");
+    __ct_vtable_dump(nullptr, "ct_vtable_diag_null.cpp:7:5", "Base");
+    return 0;
+}
