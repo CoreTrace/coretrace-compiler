42bcab580721fdad9c8bd31a5351e41c560cdd9e
Hugo <hugo.payet@epitech.eu>
2026-02-03 18:48:32 +0100

    chore(test): remove bad comments

--- test/ct_autofree_local.c
+++ test/ct_autofree_local.c
@@ -47,18 +47,3 @@ int main(void)
     // free(global2);
     return 0;
 }
-
-// `malloc(sizeof(void*));` -> pas stocké -> unreachable -> autofree instant
-// `void* ptr_leaked = malloc(sizeof(void*));` -> stocké -> reachable dans le scope de la fonction (à préciser où il est reachable)
-//      Si il est stocké dans une variable global -> mises à jour du status reachable func à reachable global.
-//      Si il reste dans la fonction -> pas de free -> check à la fin de la fonction le status reachable global/local -> si local autofree à la fin de la fonction
-//      -> si global = ne rien faire
-
-// CT_AUTOFREE_SCAN_PTR=1 \
-// CT_AUTOFREE_SCAN_GLOBALS=0 \
-// CT_AUTOFREE_SCAN_STACK=1 \
-// CT_AUTOFREE_SCAN_REGS=0 \
-// CT_AUTOFREE_SCAN_PERIOD_NS=10 \
-// CT_AUTOFREE_SCAN_BUDGET_MS=1000 \
-// CT_DEBUG_AUTOFREE_SCAN=1 CT_AUTOFREE_SCAN_INTERIOR=0 \
-// /tmp/ct_test
